5864bb0f7da59a93a5aaf434a2a8477e3556ea80
Matthew Lin <Lin.M@etoncollege.org.uk>
2024-02-22 21:39:08 +0000

    Initial Commit

---
+++ Absence Recorder/Absence_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Absence_RecorderApp.swift
+//  Absence Recorder
+//
+//  Created by Lin, Matthew (CDNP) on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Absence_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Absence Recorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Absence Recorder
+//
+//  Created by Lin, Matthew (CDNP) on 22/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
